4a8b2ea4db3c074377db58f732d5d5ebf0059ab6
Rupesh Kumar Raushan <rupeshkumarraushan0@gmail.com>
2024-12-29 07:18:25 +0530

    Utils setup

---
+++ src/controllers/userControllers/user.controllers.js
@@ -0,0 +1,8 @@
+import { User } from '../../models/users.models.js'
+import { asyncHandler } from '../../utils/asyncHandler.js'
+
+const handleUserSignup = asyncHandler( async (req, res) => {})
+
+export {
+    handleUserSignup
+}
